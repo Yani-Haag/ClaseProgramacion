6af634638fe979288f24c945485c775450700889
Yani-Haag <104107528+Yani-Haag@users.noreply.github.com>
2022-04-29 23:09:23 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+let dato = document.getElementById("dato");
+let btnEnviar = document.getElementById("enviar");
+
+btnEnviar.addEventListener("click" , () => {
+
+  let datoExtraido: number = Number(dato.value);
+
+  if (datoExtraido > 20) {
+    console.log("El numero es mayor a 20")
+  } else {
+    console.log("el numero es menor o igual que 20")
+  }
+})
